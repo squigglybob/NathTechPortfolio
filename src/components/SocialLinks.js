8b31ff77df85f90c387d06c3092f17fc7d5eb87f
Nathaniel Fenton <nathinabob@gmail.com>
2020-04-17 15:15:00 +0100

    social links and 404 change

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import 'assets/styles/components/SocialLinks.scss'
+
+const socialLinks = [
+    {
+        name: 'LinkedIn',
+        link: 'linkedIn',
+        icon: 'https://picsum.photos/50/50',
+    },
+    {
+        name: 'GitHub',
+        link: 'GitHub',
+        icon: 'https://picsum.photos/50/50',
+    }
+]
+
 export default function SocialLinks() {
     return (
-        <div>
-            SocialLinks
+        <div className="social-links">
+            {socialLinks.map((link, i) =>
+                <a href={link.link} className="social-links__link">
+                    <img className="social-links__icon" src={link.icon} alt={link.name} title={link.name} />
+                </a>
+            )}
         </div>
     )
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import Layout from '../components/Layout'
+import Layout from 'components/Layout'
+import Wrapper from 'components/Wrapper'
 
 class NotFoundPage extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Wrapper>
+          <h1>Not Found</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </Wrapper>
       </Layout>
     )
   }
